11a988aafaf898eb322b6dfab9185aed9c828d0a
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-02-10 21:26:56 -0500

    chore: upgrade ponder, use multichain

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,15 +4,16 @@ import { http } from "viem";
 import { PoolManagerAbi } from "./abis/PoolManager";
 
 export default createConfig({
+  ordering: "multichain",
   networks: {
     mainnet: {
       chainId: 1,
       transport: http(process.env.PONDER_RPC_URL_1),
     },
-    unichain: {
-      chainId: 130,
-      transport: http(process.env.PONDER_RPC_URL_130),
-    },
+    // unichain: {
+    //   chainId: 130,
+    //   transport: http(process.env.PONDER_RPC_URL_130),
+    // },
     arbitrum: {
       chainId: 42161,
       transport: http(process.env.PONDER_RPC_URL_42161),
@@ -40,15 +41,16 @@ export default createConfig({
   },
   contracts: {
     PoolManager: {
+      abi: PoolManagerAbi,
       network: {
         mainnet: {
           address: "0x000000000004444c5dc75cb358380d2e3de08a90",
-          startBlock: 21688329
+          startBlock: 21688329,
         },
-        unichain: {
-          address: "0x1f98400000000000000000000000000000000004",
-          startBlock: 0
-        },
+        // unichain: {
+        //   address: "0x1f98400000000000000000000000000000000004",
+        //   startBlock: 0,
+        // },
         arbitrum: {
           address: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
           startBlock: 297842872,
@@ -72,20 +74,8 @@ export default createConfig({
         blast: {
           address: "0x1631559198a9e474033433b2958dabc135ab6446",
           startBlock: 14377311,
-
         },
       },
-      abi: PoolManagerAbi,
-      filter: [
-        {
-          event: "Initialize",
-          args: {}
-        },
-        {
-          event: "Swap",
-          args: {}
-        },
-      ],
     },
   },
 });
