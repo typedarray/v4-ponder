44930943d75861492bd903de72ecac5c6a9e527a
Kyle Scott <kyscott@umich.edu>
2025-02-24 17:39:42 -0500

    update ponder

--- ponder.config.ts
+++ ponder.config.ts
@@ -9,34 +9,42 @@ export default createConfig({
     mainnet: {
       chainId: 1,
       transport: http(process.env.PONDER_RPC_URL_1),
+      maxRequestsPerSecond: 400,
     },
-    // unichain: {
-    //   chainId: 130,
-    //   transport: http(process.env.PONDER_RPC_URL_130),
-    // },
+    unichain: {
+      chainId: 130,
+      transport: http(process.env.PONDER_RPC_URL_130),
+      maxRequestsPerSecond: 400,
+    },
     arbitrum: {
       chainId: 42161,
       transport: http(process.env.PONDER_RPC_URL_42161),
+      maxRequestsPerSecond: 400,
     },
     optimism: {
       chainId: 10,
       transport: http(process.env.PONDER_RPC_URL_10),
+      maxRequestsPerSecond: 400,
     },
     base: {
       chainId: 8453,
       transport: http(process.env.PONDER_RPC_URL_8453),
+      maxRequestsPerSecond: 400,
     },
     polygon: {
       chainId: 137,
       transport: http(process.env.PONDER_RPC_URL_137),
+      maxRequestsPerSecond: 400,
     },
     worldchain: {
       chainId: 480,
       transport: http(process.env.PONDER_RPC_URL_480),
+      maxRequestsPerSecond: 400,
     },
     blast: {
       chainId: 81457,
       transport: http(process.env.PONDER_RPC_URL_81457),
+      maxRequestsPerSecond: 400,
     },
   },
   contracts: {
@@ -47,10 +55,10 @@ export default createConfig({
           address: "0x000000000004444c5dc75cb358380d2e3de08a90",
           startBlock: 21688329,
         },
-        // unichain: {
-        //   address: "0x1f98400000000000000000000000000000000004",
-        //   startBlock: 0,
-        // },
+        unichain: {
+          address: "0x1f98400000000000000000000000000000000004",
+          startBlock: 0,
+        },
         arbitrum: {
           address: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
           startBlock: 297842872,
