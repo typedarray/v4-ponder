72993b6024452eb0897a185805b30d7eca8311c7
Kyle Scott <kyscott@umich.edu>
2025-03-18 16:12:55 -0400

    switch schema

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,5 +1,23 @@
 import { index, relations, primaryKey, onchainTable } from "ponder";
 
+export const swap = onchainTable("swap", (t) => ({
+  id: t.text().primaryKey(),
+  poolId: t.hex().notNull(),
+  sender: t.hex().notNull(),
+  amount0: t.bigint().notNull(),
+  amount1: t.bigint().notNull(),
+  sqrtPriceX96: t.bigint().notNull(),
+  liquidity: t.bigint().notNull(),
+  tick: t.integer().notNull(),
+  fee: t.integer().notNull(),
+  chainId: t.integer().notNull(),
+}),
+  (table) => ({
+    poolIdIndex: index().on(table.poolId),
+    senderIndex: index().on(table.sender),
+  })
+);
+
 export const pool = onchainTable("pool", (t) => ({
   poolId: t.hex().notNull(),
   currency0: t.hex().notNull(),
@@ -19,23 +37,7 @@ export const poolRelations = relations(pool, ({ many }) => ({
   swaps: many(swap),
 }));
 
-export const swap = onchainTable("swap", (t) => ({
-  id: t.text().primaryKey(),
-  poolId: t.hex().notNull(),
-  sender: t.hex().notNull(),
-  amount0: t.bigint().notNull(),
-  amount1: t.bigint().notNull(),
-  sqrtPriceX96: t.bigint().notNull(),
-  liquidity: t.bigint().notNull(),
-  tick: t.integer().notNull(),
-  fee: t.integer().notNull(),
-  chainId: t.integer().notNull(),
-}),
-  (table) => ({
-    poolIdIndex: index().on(table.poolId),
-    senderIndex: index().on(table.sender),
-  })
-);
+
 
 export const swapRelations = relations(swap, ({ one }) => ({
   pool: one(pool, { fields: [swap.poolId], references: [pool.poolId] }),
